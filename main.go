dd56c0ea2fc8af17463ab2df792b078473e7106c
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 00:06:08 +0545

    setup AuthRoutes

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-crud/configs"
+	"go-crud/routes"
+)
+
+func main() {
+	SetupAppRouter()
+}
+
+func SetupAppRouter() *gin.Engine {
+
+	db := configs.Connection()
+
+	router := gin.Default()
+
+	gin.SetMode(gin.TestMode)
+
+	api := router.Group("api/v1")
+
+	routes.InitAuthRoutes(db, api)
+
+	return router
+}
