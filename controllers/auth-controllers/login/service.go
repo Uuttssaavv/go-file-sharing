1d93aa6d2535d13e4378910e64787d14c88f9a97
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 12:28:48 +0545

    added comments

--- controllers/auth-controllers/login/service.go
+++ controllers/auth-controllers/login/service.go
@@ -5,18 +5,26 @@ import (
 	 "go-crud/models"
 )
 
+// The `Service` interface defines a contract for the service
+// it has only one function `LoginService` which takes the LoginInput and returns the UserEntity and string
+
 type Service interface {
 	LoginService(input *LoginInput) (*models.UserEntity, string)
 }
 
+// The `service` struct is the concrete implementation of the `Service` interface.
 type service struct {
 	repository Repository
 }
 
+// the `NewServiceLogin` function is the constructor of the `service` struct that
+// creates the new instance of the `service` struct
 func NewServiceLogin(repository Repository) *service {
 	return &service{repository: repository}
 }
 
+// The LoginService method of the service struct implements the LoginService method
+// from the Service interface.
 func (s *service) LoginService(input *LoginInput)(*models.UserEntity, string) {
 	// TODO: concrete implementation
 	return nil,""
