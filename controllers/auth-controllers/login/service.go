9da2a531c2f049742a5cf963a5211a63a07fdc97
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:30:20 +0545

    login service completed

--- controllers/auth-controllers/login/service.go
+++ controllers/auth-controllers/login/service.go
@@ -1,15 +1,14 @@
 package loginAuth
 
-
 import (
-	 "go-crud/models"
+	"go-crud/models"
 )
 
 // The `Service` interface defines a contract for the service
-// it has only one function `LoginService` which takes the LoginInput and returns the UserEntity and string
+// it has only one function `LoginService` which takes the LoginInput and returns the UserEntity and int
 
 type Service interface {
-	LoginService(input *LoginInput) (*models.UserEntity, string)
+	LoginService(input *LoginInput) (*models.UserEntity, int)
 }
 
 // The `service` struct is the concrete implementation of the `Service` interface.
@@ -25,7 +24,10 @@ func NewServiceLogin(repository Repository) *service {
 
 // The LoginService method of the service struct implements the LoginService method
 // from the Service interface.
-func (s *service) LoginService(input *LoginInput)(*models.UserEntity, string) {
-	// TODO: concrete implementation
-	return nil,""
+func (s *service) LoginService(input *LoginInput) (*models.UserEntity, int) {
+	userEntity:= models.UserEntity{
+		Email: input.Email,
+		Password: input.Password,
+	}
+	return s.repository.LoginRepository(&userEntity);
 }
