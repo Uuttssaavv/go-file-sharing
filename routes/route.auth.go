2c3f6e3dd0e0ec821cd5784de3ee62424f061994
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 12:13:12 +0545

    created routes for authentication

--- routes/route.auth.go
+++ routes/route.auth.go
@@ -1,12 +1,17 @@
 package routes
 
 import (
+	"go-crud/controllers/auth-controllers/login"
+	"go-crud/handlers/auth-handlers/login"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
-
-
 func InitAuthRoutes(db *gorm.DB, route *gin.RouterGroup) {
+	loginRepository := loginAuth.NewRepositoryLogin(db)
+	loginService := loginAuth.NewServiceLogin(loginRepository)
+	loginHandler := loginHandler.NewHandlerLogin(loginService)
 
+	route.POST("/login", loginHandler.LoginHandler)
 }
