b52457619b95968a114d3e8df11991d82fc8a5a1
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 12:25:59 +0545

    added comments

--- controllers/auth-controllers/login/input.go
+++ controllers/auth-controllers/login/input.go
@@ -1,6 +1,8 @@
 package loginAuth
 
-type LoginInput struct{
+// LoginInput provides a way to define validation rules and
+// customize the JSON representation of struct fields.
+type LoginInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
