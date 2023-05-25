7772b795c933f7ae3411b9bc08abf431b89fe3b5
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:49:01 +0545

    login feature completed

--- handlers/auth-handlers/login/login.go
+++ handlers/auth-handlers/login/login.go
@@ -2,8 +2,12 @@ package loginHandler
 
 import (
 	"go-crud/controllers/auth-controllers/login"
+	"go-crud/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	gpc "github.com/restuwahyu13/go-playground-converter"
+	"github.com/sirupsen/logrus"
 )
 
 type handler struct {
@@ -15,5 +19,60 @@ func NewHandlerLogin(service loginAuth.Service) *handler {
 }
 
 func (h *handler) LoginHandler(ctx *gin.Context) {
-	// TODO: concrete implementation of handler
+	var input loginAuth.LoginInput
+	ctx.ShouldBindJSON(&input)
+
+	config := gpc.ErrorConfig{
+		Options: []gpc.ErrorMetaConfig{
+			{
+				Tag:     "required",
+				Field:   "Email",
+				Message: "email is required on body",
+			},
+			{
+				Tag:     "email",
+				Field:   "Email",
+				Message: "email format is not valid",
+			},
+			{
+				Tag:     "required",
+				Field:   "Password",
+				Message: "password is required on body",
+			},
+		},
+	}
+	errResponse, errCount := utils.GoValidator(&input, config.Options)
+
+	if errCount > 0 {
+		utils.ValidatorErrorResponse(ctx, http.StatusBadRequest, http.MethodPost, errResponse)
+		return
+	}
+	resultLogin, errLogin := h.service.LoginService(&input)
+
+	switch errLogin {
+
+	case http.StatusNotFound:
+		utils.APIResponse(ctx, "User account is not registered", http.StatusNotFound, http.MethodPost, nil)
+		return
+
+	case http.StatusUnauthorized:
+		utils.APIResponse(ctx, "Username or password is wrong", http.StatusForbidden, http.MethodPost, nil)
+		return
+
+	case http.StatusAccepted:
+		accessTokenData := map[string]interface{}{"id": resultLogin.ID, "email": resultLogin.Email}
+		accessToken, errToken := utils.Sign(accessTokenData, "JWT_SECRET", 24*60*1)
+
+		if errToken != nil {
+			defer logrus.Error(errToken.Error())
+			utils.APIResponse(ctx, "Generate accessToken failed", http.StatusBadRequest, http.MethodPost, nil)
+			return
+		}
+		utils.APIResponse(ctx, "Login successfully", http.StatusOK, http.MethodPost, map[string]string{"accessToken": accessToken})
+		return
+
+	default:
+
+		utils.APIResponse(ctx, "Unknown error occured", http.StatusInternalServerError, http.MethodPost, nil)
+	}
 }
