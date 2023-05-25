ade9507b37fedfb3d78519e900e53ad68226defc
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:49:18 +0545

    crated jwt util to sign the JWT

---
+++ utils/jwt.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+func Sign(Data map[string]interface{}, SecrePublicKeyEnvName string, ExpiredAt time.Duration) (string, error) {
+
+	expiredAt := time.Now().Add(time.Duration(time.Minute) * ExpiredAt).Unix()
+
+	jwtSecretKey := GodotEnv(SecrePublicKeyEnvName)
+
+	claims := jwt.MapClaims{}
+	claims["exp"] = expiredAt
+	claims["authorization"] = true
+
+	for i, v := range Data {
+		claims[i] = v
+	}
+
+	to := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	accessToken, err := to.SignedString([]byte(jwtSecretKey))
+
+	if err != nil {
+		logrus.Error(err.Error())
+		return accessToken, err
+	}
+
+	return accessToken, nil
+}
