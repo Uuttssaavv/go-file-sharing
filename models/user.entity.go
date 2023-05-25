8ae59a71cc9e2d46d6cf70de9cd0f61e71f1fe4f
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-26 01:48:03 +0545

    added before create and before update to the userEntity

--- models/user.entity.go
+++ models/user.entity.go
@@ -1,14 +1,29 @@
 package models
 
-import "time"
-
+import (
+	"github.com/jinzhu/gorm"
+	"go-crud/utils"
+	"time"
+)
 
 type UserEntity struct {
-	ID           uint    `gorm:"primary_key"`
-	Username     string  `gorm:"column:username;unique;not null"`
-	Email        string  `gorm:"column:email;unique;not null"`
-	Image        *string `gorm:"column:image"`
-	Password string  `gorm:"column:password;not null"`
+	ID        uint    `gorm:"primary_key"`
+	Username  string  `gorm:"column:username;unique;not null"`
+	Email     string  `gorm:"column:email;unique;not null"`
+	Image     *string `gorm:"column:image"`
+	Password  string  `gorm:"column:password;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (entity *UserEntity) BeforeCreate(db *gorm.DB) error {
+	entity.Password = utils.HashPassword(entity.Password)
+	entity.CreatedAt = time.Now().Local()
+	return nil
+}
+
+
+func (entity *UserEntity) BeforeUpdate(db *gorm.DB) error {
+	entity.UpdatedAt = time.Now().Local()
+	return nil
 }
