641238d321d9057ec0abcb2166449a029557751c
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-26 01:46:42 +0545

    created input for register

---
+++ controllers/auth-controllers/register/input.go
@@ -0,0 +1,7 @@
+package register
+
+type RegisterInput struct{
+	Username string `json:"username" validate:"required,lowercase"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,gte=8"`
+}
