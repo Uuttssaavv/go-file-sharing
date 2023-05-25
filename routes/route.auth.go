6716e319f808c8eb6db95996db90f4a9554047bd
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-26 01:48:13 +0545

    added routes for register

--- routes/route.auth.go
+++ routes/route.auth.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"go-crud/controllers/auth-controllers/login"
+	"go-crud/controllers/auth-controllers/register"
 	"go-crud/handlers/auth-handlers/login"
+	registerHandler "go-crud/handlers/auth-handlers/register"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -13,5 +15,12 @@ func InitAuthRoutes(db *gorm.DB, route *gin.RouterGroup) {
 	loginService := loginAuth.NewServiceLogin(loginRepository)
 	loginHandler := loginHandler.NewHandlerLogin(loginService)
 
+	registerRepository := register.NewRegisterRepository(db)
+	registerService := register.NewRegisterService(registerRepository)
+	registerHandlers := registerHandler.NewHandlerRegister(registerService)
+
+
 	route.POST("/login", loginHandler.LoginHandler)
+
+	route.POST("/register", registerHandlers.RegisterHandler)
 }
