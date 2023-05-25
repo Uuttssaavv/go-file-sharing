2932cce596e415fc0291b71b9966c4f697e53787
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:28:29 +0545

    login repository completed

--- controllers/auth-controllers/login/repository.go
+++ controllers/auth-controllers/login/repository.go
@@ -1,14 +1,17 @@
 package loginAuth
 
 import (
-	"github.com/jinzhu/gorm"
 	"go-crud/models"
+	"go-crud/utils"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
 )
 
 // The `Repository` interface defines a contract for the repository responsible for handling login-related operations
-// it has only one function `LoginRepository` which takes the UserEntity and returns the UserEntity and string
+// it has only one function `LoginRepository` which takes the UserEntity and returns the UserEntity and int
 type Repository interface {
-	LoginRepository(input *models.UserEntity) (*models.UserEntity, string)
+	LoginRepository(input *models.UserEntity) (*models.UserEntity, int)
 }
 
 // The `repository` struct is the concrete implementation of the `Repository` interface.
@@ -24,7 +27,21 @@ func NewRepositoryLogin(db *gorm.DB) *repository {
 
 // The LoginRepository method of the repository struct implements the LoginRepository method
 // from the Repository interface.
-func (r *repository) LoginRepository(input *models.UserEntity) (*models.UserEntity, string) {
-	// TODO: concrete implementation
-	return nil, ""
+func (r *repository) LoginRepository(input *models.UserEntity) (*models.UserEntity, int) {
+	//  check if the user exist
+	var users models.UserEntity
+	db := r.db.Model(&users)
+	checkAccount := db.Select("*").Where("email=?", input.Email).Find(&users)
+
+	if checkAccount.RowsAffected==0{
+		return nil, http.StatusNotFound
+	}
+	// check if the password matches
+	comparePassword := utils.ComparePassword(users.Password, input.Password)
+	
+	if comparePassword !=nil{
+		return nil, http.StatusUnauthorized
+	}
+
+	return &users,http.StatusAccepted
 }
