b8aa94be9024fe009dedf381e668e7170b24e78a
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 22:28:33 +0545

    compelted register service

--- controllers/auth-controllers/register/service.go
+++ controllers/auth-controllers/register/service.go
@@ -9,18 +9,18 @@ type Service interface {
 }
 
 type service struct {
-	r Repository
+	repository Repository
 }
 
 func NewRegisterService(repository *repository) *service {
-	return &service{r: repository}
+	return &service{repository: repository}
 }
 
-func (s *service) RegisterService(input *RegisterInput) (*models.UserEntity, int) {
+func (service *service) RegisterService(input *RegisterInput) (*models.UserEntity, int) {
 	user := models.UserEntity{
 		Email:    input.Email,
 		Password: input.Password,
 		Username: input.Username,
 	}
-	return s.r.RegisterRepository(&user)
+	return service.repository.RegisterRepository(&user)
 }
