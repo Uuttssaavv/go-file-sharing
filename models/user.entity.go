5316432132b23e7c92166b70653479183df42b16
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:30:59 +0545

    changed pwHash to password to make it more readable

--- models/user.entity.go
+++ models/user.entity.go
@@ -5,11 +5,10 @@ import "time"
 
 type UserEntity struct {
 	ID           uint    `gorm:"primary_key"`
-	Username     string  `gorm:"column:usernameunique;not null"`
+	Username     string  `gorm:"column:username;unique;not null"`
 	Email        string  `gorm:"column:email;unique;not null"`
 	Image        *string `gorm:"column:image"`
-	PasswordHash string  `gorm:"column:password;not null"`
-	AccessKey string  `gorm:"column:access_key;unique;not null"`
+	Password string  `gorm:"column:password;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
