f2bdc0bb7b431baf225bd6bf7218ac3cc276e9ff
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-26 01:48:26 +0545

    added password hash function

--- utils/bcrypt.go
+++ utils/bcrypt.go
@@ -1,10 +1,22 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"github.com/sirupsen/logrus"
+)
 
 func ComparePassword(hashPassword string, password string) error {
 	pw := []byte(password)
 	hw := []byte(hashPassword)
 	err := bcrypt.CompareHashAndPassword(hw, pw)
 	return err
+}
+
+func HashPassword(password string) string {
+	pw := []byte(password)
+	result, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
+	if err != nil {
+		logrus.Fatal(err.Error())
+	}
+	return string(result)
 }
