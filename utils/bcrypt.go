f96dafd03ebb3856efe29f3b85a42a23cae5f7f9
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-25 21:28:17 +0545

    added password compare utils

---
+++ utils/bcrypt.go
@@ -0,0 +1,10 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func ComparePassword(hashPassword string, password string) error {
+	pw := []byte(password)
+	hw := []byte(hashPassword)
+	err := bcrypt.CompareHashAndPassword(hw, pw)
+	return err
+}
