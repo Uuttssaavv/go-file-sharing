439bc9f9d274c67a5b7bf850ef16e7739809709d
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-28 00:50:14 +0545

    added file service

--- controllers/file-controllers/service.go
+++ controllers/file-controllers/service.go
@@ -1,1 +1,36 @@
 package filecontrollers
+
+import "go-crud/models"
+
+type Service interface {
+	CreateFile(input *FileInput) (*models.FileModel, int)
+
+	GetAllFiles() ([]models.FileModel, int)
+
+	DeleteFile(fileID uint) int
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewFileService(r Repository) *service {
+	return &service{repository: r}
+}
+
+func (s *service) CreateFile(input *FileInput) (*models.FileModel, int) {
+	fileModel := models.FileModel{
+		Type: input.Type,
+		Name: input.Name,
+		Url:  input.Url,
+	}
+	return s.repository.CreateFile(&fileModel)
+}
+func (s *service) GetAllFiles() ([]models.FileModel, int) {
+
+	return s.repository.GetAllFiles()
+}
+func (s *service) DeleteFile(fileID uint) int {
+
+	return s.repository.DeleteFile(fileID)
+}
