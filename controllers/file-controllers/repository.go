2a2231beb02c08ad0f283df978e824d94302aa50
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-28 00:50:22 +0545

    added  file repository

--- controllers/file-controllers/repository.go
+++ controllers/file-controllers/repository.go
@@ -1,11 +1,38 @@
 package filecontrollers
 
-import "go-crud/models"
+import (
+	"go-crud/models"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Repository interface {
-	CreateFile(file *models.FileModel) (models.FileModel, int)
+	CreateFile(file *models.FileModel) (*models.FileModel, int)
 
 	GetAllFiles() ([]models.FileModel, int)
 
 	DeleteFile(fileID uint) int
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewFileRepository(db *gorm.DB) *repository {
+	return &repository{db: db}
+}
+
+func (repo *repository) CreateFile(file *models.FileModel) (*models.FileModel, int) {
+
+	return nil, 0
+}
+
+func (repo *repository) GetAllFiles() ([]models.FileModel, int) {
+
+	return nil, 0
+}
+
+func (repo *repository) DeleteFile(fileID uint) int {
+
+	return 0
 }
