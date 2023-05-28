2f20db7c3f9c72c04ba009a9e689dedb1c9898fa
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-29 00:06:08 +0545

    added json keys

--- models/file.entity.go
+++ models/file.entity.go
@@ -2,12 +2,12 @@ package models
 
 import "time"
 
-type FileModel struct{
-	ID uint `gorm:"primary_key"`
-	Type string `gorm:"not null"`
-	Name string `gorm:"not null"`
-	Url string `gorm:"not null"`
-	AccessKey string `gorm:""`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+type FileModel struct {
+	ID        uint      `gorm:"primary_key" json:"id"`
+	Type      string    `gorm:"not null" json:"type"`
+	Name      string    `gorm:"not null" json:"name"`
+	Url       string    `gorm:"not null" json:"url"`
+	AccessKey string    `gorm:"" json:"access_key"`
+	CreatedAt time.Time `gorm:"" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"" json:"updatedAt"`
 }
