d060277515f2bc76421cbfb2b5033628a6a984b9
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-28 23:35:58 +0545

    created cloudinary utils to upload and access files

---
+++ utils/files.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"mime/multipart"
+
+	"github.com/cloudinary/cloudinary-go"
+	"github.com/cloudinary/cloudinary-go/api/admin"
+	"github.com/cloudinary/cloudinary-go/api/uploader"
+)
+
+func UploadFile(file multipart.File, fileType string) (*uploader.UploadResult, error) {
+	cld, _ := cloudinary.NewFromURL(GodotEnv("CLOUDINARY_URL"))
+
+	randString := GenerateRandomString(6)
+	fileName := fmt.Sprintf("uploads/%s", randString)
+	fmt.Println(fileType)
+	result, err := cld.Upload.Upload(context.Background(), file, uploader.UploadParams{
+		PublicID: fileName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func GetFileUrl(fileId string) string {
+	cld, _ := cloudinary.NewFromURL(GodotEnv("CLOUDINARY_URL"))
+
+	result, err := cld.Admin.Asset(context.Background(), admin.AssetParams{
+		PublicID: fileId,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return err.Error()
+	}
+	fmt.Println(result)
+	return result.URL
+}
