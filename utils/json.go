897d166fd06c1ed1899d719584639c35b331b745
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-29 22:52:20 +0545

    added StringToEntity

--- utils/json.go
+++ utils/json.go
@@ -10,3 +10,9 @@ func ObjectToJson[T any](object interface{}, data *T) {
 
 	json.Unmarshal([]byte(jason), &data)
 }
+
+func StringToEntity[T any](value any, object *T) error {
+	data, _ := json.Marshal(value)
+	errors := json.Unmarshal([]byte(data), &object)
+	return errors
+}
