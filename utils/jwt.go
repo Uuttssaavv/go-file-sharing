1f5dfad29951e3d0a4eb00dc0e64c5e62a2ec5c9
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-29 21:50:08 +0545

    added token verify and decoder

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,10 +1,25 @@
 package utils
 
 import (
-	"github.com/dgrijalva/jwt-go"
-	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"strings"
 	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
+
+type MetaToken struct {
+	ID            string
+	Email         string
+	ExpiredAt     time.Time
+	Authorization bool
+}
+
+type AccessToken struct {
+	Claims MetaToken
+}
 
 func Sign(Data map[string]interface{}, SecrePublicKeyEnvName string, ExpiredAt time.Duration) (string, error) {
 
@@ -29,4 +44,44 @@ func Sign(Data map[string]interface{}, SecrePublicKeyEnvName string, ExpiredAt t
 	}
 
 	return accessToken, nil
+}
+
+func VerifyTokenHeader(ctx *gin.Context, SecrePublicKeyEnvName string) (*jwt.Token, error) {
+	tokenHeader := ctx.GetHeader("Authorization")
+	accessToken := strings.SplitAfter(tokenHeader, "Bearer")[1]
+	jwtSecretKey := GodotEnv(SecrePublicKeyEnvName)
+
+	token, err := jwt.Parse(strings.Trim(accessToken, " "), func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecretKey), nil
+	})
+
+	if err != nil {
+		logrus.Error(err.Error())
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func VerifyToken(accessToken, SecrePublicKeyEnvName string) (*jwt.Token, error) {
+	jwtSecretKey := GodotEnv(SecrePublicKeyEnvName)
+
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecretKey), nil
+	})
+
+	if err != nil {
+		logrus.Error(err.Error())
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func DecodeToken(accessToken *jwt.Token) AccessToken {
+	var token AccessToken
+	stringify, _ := json.Marshal(&accessToken)
+	json.Unmarshal([]byte(stringify), &token)
+
+	return token
 }
