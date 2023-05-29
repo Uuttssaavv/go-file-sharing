b039d704cff2a246a3a9092770c9f526e5cdcb82
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-29 22:54:57 +0545

    added foreign key to the file entity

--- models/file.entity.go
+++ models/file.entity.go
@@ -1,13 +1,27 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type FileModel struct {
-	ID        uint      `gorm:"primary_key" json:"id"`
-	Type      string    `gorm:"not null" json:"type"`
-	Name      string    `gorm:"not null" json:"name"`
-	Url       string    `gorm:"not null" json:"url"`
-	AccessKey string    `gorm:"" json:"access_key"`
-	CreatedAt time.Time `gorm:"" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"" json:"updatedAt"`
+	ID        uint       `gorm:"primary_key" json:"id"`
+	Type      string     `gorm:"not null" json:"type"`
+	Name      string     `gorm:"not null" json:"name"`
+	Url       string     `gorm:"not null" json:"url"`
+	AccessKey string     `gorm:"" json:"access_key"`
+	CreatedAt time.Time  `gorm:"" json:"createdAt"`
+	UpdatedAt time.Time  `gorm:"" json:"updatedAt"`
+	UserID    uint       `gorm:"foreignkey:UserRefer" json:"-"`
+	User      UserEntity `gorm:"foreignkey:UserRefer" json:"user"`
+}
+
+
+func (entity *FileModel) BeforeCreate(db *gorm.DB) error {
+	entity.CreatedAt = time.Now().Local()
+	entity.UpdatedAt = time.Now().Local()
+
+	return nil
 }
--- models/user.entity.go
+++ models/user.entity.go
@@ -10,7 +10,7 @@ type UserEntity struct {
 	ID        uint    `gorm:"primary_key"`
 	Username  string  `gorm:"column:username;unique;not null"`
 	Email     string  `gorm:"column:email;unique;not null"`
-	Image     *string `gorm:"column:image"`
+	Image     string `gorm:"column:image"`
 	Password  string  `gorm:"column:password;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
