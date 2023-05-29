5e5b83263c80554c292f8e26954d464cdcd80209
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-29 22:53:00 +0545

    formatted

--- controllers/file-controllers/input.go
+++ controllers/file-controllers/input.go
@@ -1,8 +1,9 @@
 package filecontrollers
 
-type FileInput struct{
-	ID string `gorm:"primary_key"`
-	Type string `gorm:"not null"`
-	Name string `gorm:"not null"`
-	Url string `gorm:"not null"`
+type FileInput struct {
+	ID     string `gorm:"primary_key"`
+	Type   string `gorm:"not null"`
+	Name   string `gorm:"not null"`
+	Url    string `gorm:"not null"`
+	UserId uint   `gorm:"not null"`
 }
