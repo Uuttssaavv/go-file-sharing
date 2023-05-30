ee47e4b67aba1586eebd27a83ab349ce25178164
Uuttssaavv <ghimireutsav11@gmail.com>
2023-05-30 19:17:34 +0545

    return nil for failure condition

--- tests/mocks/mocks.go
+++ tests/mocks/mocks.go
@@ -1,15 +1,19 @@
 package mocks
 
 import (
-	"github.com/stretchr/testify/mock"
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/mock"
 )
 
 type MockDBConnection struct {
 	mock.Mock
+	ReturnNil bool
 }
 
 func (m *MockDBConnection) Connection() *gorm.DB {
 	args := m.Called()
+	if args.Get(0) == true {
+		return nil
+	}
 	return args.Get(0).(*gorm.DB)
 }
